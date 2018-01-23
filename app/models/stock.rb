ced90e3381a9e1f8928224d9bf7b670281f4ab5e
Andrew Mason <astrosurfer888@gmail.com>
2018-01-23 10:03:19 -0500

    CRDC #167: Invalid Search Results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,17 @@
 class Stock < ActiveRecord::Base
   def self.new_from_lookup(ticker_symbol)
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    price = strip_commas(looked_up_stock.l)
-    # Constructs new object and sends back to whoever called it
-    new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      price = strip_commas(looked_up_stock.l)
+      # Constructs new object and sends back to whoever called it
+      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol,
+          last_price: price)
+    rescue Exception => e
+      return nil
+    end
   end
 
-  def self.strip_commas(number) 
+  def self.strip_commas(number)
     number.gsub(",","")
   end
 end
