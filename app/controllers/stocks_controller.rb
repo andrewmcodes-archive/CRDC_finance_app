13c19f66e815bede18c5829f1215aeb64b368ffb
Andrew Mason <astrosurfer888@gmail.com>
2018-01-23 11:55:01 -0500

    CRDC #169: Ajax for form submission

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,8 +3,10 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_from_lookup(params[:stock])
-      if @stock 
-        render 'users/my_portfolio'
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
         flash[:danger] = 'You have entered an invalid symbol'
         redirect_to my_portfolio_path
