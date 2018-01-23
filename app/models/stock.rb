df7cae04c0e445c46be72643f26aa803fcc8cf58
Andrew Mason <astrosurfer888@gmail.com>
2018-01-23 14:09:57 -0500

    CRDC #177: Stock Restrictions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,12 @@
+# Stock Model
 class Stock < ActiveRecord::Base
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
   def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
@@ -12,6 +20,6 @@ class Stock < ActiveRecord::Base
   end
 
   def self.strip_commas(number)
-    number.gsub(",","")
+    number.gsub(',', '')
   end
 end
