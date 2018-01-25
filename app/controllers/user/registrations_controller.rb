8c9cc47abdbd91f39104e8e86443eb10ea0b2fd1
Andrew Mason <astrosurfer888@gmail.com>
2018-01-25 13:30:38 -0500

    CRDC #181: User Details

---
+++ app/controllers/user/registrations_controller.rb
@@ -0,0 +1,11 @@
+# Registration Controller
+class User::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanatizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanatizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+end
